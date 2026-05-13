a4965067a7b41d7101ea4b73a586ddaf70a62ab6
jeswin13 <jeswindersingh36@gmail.com>
2026-05-13 08:25:01 +0700

    Update soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
+int main(){
+    int n,m;
+    char pulau [100][100];
+    int b = 0;
+
+    scanf ("%d",&n);
+    scanf ("%d", &m);
+
+    for (int i = 0; i < n; i++){
+        scanf ("%s",pulau[i]);
+
+        int a =0;
+        for (int j = 0; j <m; j ++){
+        if (pulau[i][j] =='1'){
+                a++;
+        if (a > b){
+                b = a;
+        }
+        }
+    }
+}
+
+    printf ("ISLANDS %d\n",n);
+    printf ("LARGEST %d\n",b);
     return 0;
 }
